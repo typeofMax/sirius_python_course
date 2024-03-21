197280debd1bcdc5f6f074f20d5e52f9dd20185d
typeofMax <work@typeofmax.ru>
2024-03-21 23:21:35 +0300

    complete task

---
+++ 4-if-condition/task-3.py
@@ -0,0 +1,6 @@
+k, l, m, n = int(input()), int(input()), int(input()), int(input())
+
+if k == m or l == n:
+    print('YES')
+else:
+    print('NO')
---
+++ 4-if-condition/task-4.py
@@ -0,0 +1,6 @@
+a1, b1, a2, b2 = int(input()), int(input()), int(input()), int(input())
+
+if (a1 == b1 and a2 == b2) or (a1 + b1 == a2 + b2):
+    print('YES')
+else:
+    print('NO')
